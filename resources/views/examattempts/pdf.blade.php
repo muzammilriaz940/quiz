239bf0f3b444242240a6cde67355d33c3ba0e3c2
Muzammil Riaz <muzammil.riaz940@eplanetcom.com>
2022-07-04 16:24:49 +0500

    updates

--- resources/views/examattempts/pdf.blade.php
+++ resources/views/examattempts/pdf.blade.php
@@ -31,11 +31,21 @@
         .red-dot {
           background-color: red;
         }
+
+        table {
+            font-size: 12px;
+        }
+
+        .table-border{
+            border: 1px solid black;
+            border-radius: 12px;
+        }
+
     </style>
 </head>
 
 <body>
-    <!-- <table width="100%">
+    <table width="100%">
         <tr class="text-center">
             <th>{{ $EA->exam->name }}</th>
         </tr>
@@ -61,9 +71,9 @@
             </td>
         </tr>
         @endforeach
-    </table> -->
+    </table>
 
-    <!-- <div class="page-break"></div>
+    <div class="page-break"></div>
 
     <table width="50%" class="center-table">
         <tr class="text-center">
@@ -115,6 +125,91 @@
         @endforeach
     </table>
 
-    <div class="page-break"></div> -->
+    <div class="page-break"></div>
+
+    <table width="100%">
+        <tr class="text-center">
+            <td colspan="5">Student Name: <u>{{ $EA->studentName }}</u></td>
+            <td colspan="5">Date of Test: <u>{{ $EA->studentName }}</u> </td>
+        </tr>
+        <tr class="text-left">
+            <td colspan="10">
+                <p>Hospital Scenario: “You are working in a hospital or clinic, and you see a person who has suddenly collapsed in the
+hallway. You check that the scene is safe and then approach the patient. Demonstrate what you would do next.”
+Prehospital Scenario: “You arrive on the scene for a suspected cardiac arrest. No bystander CPR has been provided. You
+approach the scene and ensure that itis safe. Demonstrate what you would do next.”</p>
+            </td>
+        </tr>
+    </table>
+
+    <table width="100%" class="table-border">
+        <tr>
+            <td colspan="10"><b>Assesments & Activation</b></td>
+        </tr>
+        <tr>
+            <td colspan="5">
+                <input type="checkbox" checked> Checks Responsiveness
+            </td>
+            <td colspan="5">
+                <input type="checkbox" checked> Shouts for help/Activates emergency response system/Sends for AED
+            </td>
+        </tr>
+        <tr>
+            <td colspan="5">
+                <input type="checkbox" checked> Checks Breathing
+            </td>
+            <td colspan="5">
+                <input type="checkbox" checked> Checks Pulse
+            </td>
+        </tr>
+    </table>
+
+    <table width="100%">
+    <tr>
+        <p>Once student shouts for help, instructor says, “Here's the barrier device. | am going to get the AED.”</p>
+    </tr>
+    </table>
+
+    <table width="100%" class="table-border">
+        <tr>
+            <td colspan="10"><b>Cycle 1 of CPR(30:2) *CPR feedback devices are required for accuracy</b></td>
+        </tr>
+        <tr>
+            <td colspan="10">
+                <input type="checkbox" checked> Adult Compressions
+                <ul>
+                    <li>Performs high-quality compressions*:</li>
+                    <li>Hand placement on lower half of sternum</li>
+                    <li>30 compressions in no less than 15 and no more than 18 seconds</li>
+                    <li>Compresses at least 2 inches (5 cm)</li>
+                    <li>Complete recoil after each compression</li>
+                </ul>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="10">
+                <input type="checkbox" checked> Adult Breaths
+                <ul>
+                    <li>Gives 2 breaths with a barrier device:</li>
+                    <li>Each breath given over 1 second</li>
+                    <li>Visible chest rise with each breath</li>
+                    <li>Resumes compressions in less than 10 seconds</li>
+                </ul>
+            </td>
+        </tr>
+    </table>
+    <br>
+    <table width="100%" class="table-border">
+        <tr>
+            <td colspan="9"><b>Cycle 2 of CPR (repeats stepsin Cycle 1) Only check box if step is successfully performed</b></td>
+        </tr>
+        <tr>
+            <td colspan="9">
+                <input type="checkbox" checked> Compressions
+                <input type="checkbox" checked> Compressions
+                <input type="checkbox" checked> Compressions
+            </td>
+        </tr>
+    </table>
 </body>
 </html>
